f066dbe65d8670d437ea7475f1db936109f1e913
Marcin Jedynski <marcin.jedynski@cognifide.com>
2018-05-30 15:16:37 +0200

    bug fix lvs2

--- lvs-2.sh
+++ lvs-2.sh
@@ -2,6 +2,8 @@ apt-get update
 apt-get install -y ipvsadm keepalived
 cat ./2keepalived.conf > /etc/keepalived/keepalived.conf
 service keepalived restart
+ifdown eth0
+ip a f dev eth0
 cat ./interfaces.lvs2 >> /etc/network/interfaces
 ifup eth0
 ifup eth1
